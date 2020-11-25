1cde55245594cd9a2657a1a20f42222d234e403a
Pranjal Enchalwar <enchalwarpranjal@gmail.com>
2020-11-25 21:26:50 +0530

    Please check api files

---
+++ lib/api/Cdzapi.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import '../main.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class CdzAPI {
+  Future<CurrencyRate> getData(String base, String target) async {
+    String convert = base + target;
+    String _url =
+        "https://marketdata.tradermade.com/api/v1/live?currency=$convert&api_key=-3F23YQDx2mgHUv6-M-m";
+
+    var responseJSON = await http.get(_url);
+    var convertedJSON = jsonDecode(responseJSON.body);
+    var data = convertedJSON["quotes"][0];
+    var buy = data["ask"];
+    var sell = data["bid"];
+    CurrencyRate currencyRate = CurrencyRate(buy.toString(), sell.toString());
+    return currencyRate;
+  }
+}
