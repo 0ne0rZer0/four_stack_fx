0dde75357a820e590434d01b37a5f32417e59b40
Pranjal Enchalwar <enchalwarpranjal@gmail.com>
2020-11-25 22:31:41 +0530

    cleaned

--- lib/api/Forexapi.dart
+++ lib/api/Forexapi.dart
@@ -5,14 +5,13 @@ import 'package:http/http.dart' as http;
 import 'package:four_stack_fx/model/currency_rate.dart';
 
 class ForexAPI {
+  String _url = "https://fcsapi.com/api-v3/forex";
+  String apiKey = "dsUx9GI5jsA4lGzHmzisLCj8"; //Apikey
   Future<CurrencyRate> getData(String base, String target) async {
     String convert = base + "/" + target;
-    String _url =
-        "https://fcsapi.com/api-v3/forex/latest?symbol=$convert&access_key=dsUx9GI5jsA4lGzHmzisLCj8";
-
-    var responseJSON = await http.get(_url);
+    var responseJSON =
+        await http.get(_url + "/latest?symbol=$convert&access_key=" + apiKey);
     var convertedJSON = jsonDecode(responseJSON.body);
-
     var data = convertedJSON["response"][0];
     var buy = data["h"];
     var sell = data["l"];
